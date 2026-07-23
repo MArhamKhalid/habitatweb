5af01edab994effb31531b316eb9d2a49053e6c4
Arham khalid <arhamkhalid79@gmail.com>
2026-07-24 02:04:54 +0500

    Update Detail.tsx

--- Components/Detail.tsx
+++ Components/Detail.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Detail = () => {
   return (
     <section className='w-full h-dvh bg-[#112931]'>
-        <div className='flex justify-between items-center py-18 gap-60'>
-            <div className='w-150 '><img src='/owner.png' /></div>
-            <div className='flex flex-col justify-center items-center text-left h-200'>
-                <h2>At Habitat Pools & Landscape, we do things differently</h2>
-                <p>When you work with us, you work directly with the owners. We’re on-site multiple times a week, overseeing every detail, making decisions in real time, and ensuring nothing gets lost in translation.</p>
-                <div>Call Us Today</div>
-            </div>
-        </div>
+      <div className='flex justify-between w-full h-full items-center py-18 gap-40'>
+          <div className='w-150 '><img className='rounded-2xl' src='/owner.png' /></div>
+          <div className='flex flex-col justify-center items-right text-left h-200 w-220 gap-8'>
+              <h2 className='text-7xl'>At Habitat Pools & Landscape,<br/> we do things differently</h2>
+              <p className='text-3xl capitalize'>When you work with us, you work directly with the owners. We’re<br/> on-site multiple times a week, overseeing every detail,<br/> making decisions in real time, and ensuring nothing<br/> gets lost in translation.</p>
+              <div><a className='btn-all '>Call Us Today</a></div>
+          </div>
+      </div>
     </section>
   )
 }
