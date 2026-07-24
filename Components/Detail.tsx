fffd5b7942bf361c35bf5b3886713292d23b0469
Arham khalid <arhamkhalid79@gmail.com>
2026-07-24 12:52:42 +0500

    updated

--- Components/Detail.tsx
+++ Components/Detail.tsx
@@ -8,7 +8,9 @@ const Detail = () => {
           <div className='flex flex-col justify-center items-right text-left h-200 w-220 gap-8'>
               <h2 className='text-7xl'>At Habitat Pools & Landscape,<br/> we do things differently</h2>
               <p className='text-3xl capitalize'>When you work with us, you work directly with the owners. We’re<br/> on-site multiple times a week, overseeing every detail,<br/> making decisions in real time, and ensuring nothing<br/> gets lost in translation.</p>
-              <div><a className='btn-all '>Call Us Today</a></div>
+              <div className='btn-all relative mt-10 '>
+                <a className='relative py-4 px-8 text-xl text-center underline'>Call Us Today</a>
+              </div>
           </div>
       </div>
     </section>
--- Components/Services.tsx
+++ Components/Services.tsx
@@ -5,13 +5,15 @@ const Services = () => {
     <section className="w-full h-dvh grid-cols-1 justify-center items-start">
         <img src='/service-01.png ' className='w-full h-full absolute object-cover' />
 
-        <div className="flex grid-rows-2 justify-between items-end w-full px-20 z-10 h-80 text-white">
+        <div className="flex grid-rows-2 justify-between items-end w-full px-20 z-10 h-120 text-white">
             <div className="text-7xl font-bold w-250 z-10">
                 <h4>Pool Design & Build</h4>
             </div>
             <div className="w-150  text-xl z-10">
                 <h3>Custom pools designed around your space, your lifestyle, and your vision—built with precision and long-term durability in mind.</h3>
-                <div className='h-16 w-48 border flex justify-center items-center mx-5 underline'>Book A service</div>
+                <div className='btn-all relative mt-20'>
+                    <a className='relative py-4 px-8 text-xl text-center underline'>Book A service</a>
+                </div>
             </div>
         </div>
     </section>
